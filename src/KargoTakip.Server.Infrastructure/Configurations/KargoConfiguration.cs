5865c203605651a0bd1bbfe099a11c58e24c0b4b
iaydemirankabeta <67390681+ismailaydemirx@users.noreply.github.com>
2025-03-04 13:13:17 +0300

    Add: Cargo Configurations
    Kargo property'leri için konfigürasyonlar eklendi.

--- src/KargoTakip.Server.Infrastructure/Configurations/KargoConfiguration.cs
+++ src/KargoTakip.Server.Infrastructure/Configurations/KargoConfiguration.cs
@@ -11,15 +11,36 @@ internal sealed class KargoConfiguration : IEntityTypeConfiguration<Kargo>
         {
             builder.Property(p => p.FirstName).HasColumnType("varchar(50)"); //nvarchar(MAX)
             builder.Property(p => p.LastName).HasColumnType("varchar(50)");
+            builder.Property(p => p.TCNumarasi).HasColumnType("varchar(11)");
+            builder.Property(p => p.Email).HasColumnType("varchar(100)");
+            builder.Property(p => p.PhoneNumber).HasColumnType("varchar(15)");
         });
-        builder.OwnsOne(p => p.Alici);
-        builder.OwnsOne(p => p.TeslimAdresi);
+        builder.OwnsOne(p => p.Alici, alici =>
+        {
+            alici.Property(p => p.FirstName).HasColumnType("varchar(50)");
+            alici.Property(p => p.LastName).HasColumnType("varchar(50)");
+            alici.Property(p => p.TCNumarasi).HasColumnType("varchar(11)");
+            alici.Property(p => p.Email).HasColumnType("varchar(100)");
+            alici.Property(p => p.PhoneNumber).HasColumnType("varchar(15)");
+        });
+        builder.OwnsOne(p => p.TeslimAdresi, adres =>
+        {
+            adres.Property(p => p.City).HasColumnType("varchar(500)");
+            adres.Property(p => p.Town).HasColumnType("varchar(50)");
+            adres.Property(p => p.Mahalle).HasColumnType("varchar(50)");
+            adres.Property(p => p.Street).HasColumnType("varchar(50)");
+            adres.Property(p => p.FullAddress).HasColumnType("varchar(200)");
+        });
         builder.OwnsOne(p => p.KargoInformation, builder =>
         {
             builder
             .Property(p => p.KargoTipi)
-            .HasConversion(tip => tip.Value, value => KargoTipiEnum.FromValue(value));
+            .HasConversion(tip => tip.Value, // Enum değerini veritabanına kaydet
+            value => KargoTipiEnum.FromValue(value)); // Veritabanındaki değeri enum'a dönüştür
+            builder.Property(p => p.Agirlik).HasColumnType("decimal(18,2)");
         });
-        builder.Property(p => p.KargoDurum).HasConversion(durum => durum.Value, value => KargoDurumEnum.FromValue(value));
+        builder.Property(p => p.KargoDurum)
+            .HasConversion(durum => durum.Value, // Enum değerini veritabanına kaydet
+            value => KargoDurumEnum.FromValue(value)); // Veritabanındaki değeri enum'a dönüştür
     }
 }
